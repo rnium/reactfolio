17c6995b2d1fba719d2e969ec162f54234b173cb
rony <rniumo@gmail.com>
2024-04-28 01:03:00 +0600

    changes

--- src/data/user.js
+++ src/data/user.js
@@ -22,7 +22,7 @@ const INFO = {
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I'm Md. Saiful Islam Rony, from Bangladesh.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -38,7 +38,7 @@ const INFO = {
 			title: "SEC UGRP",
 			description:
 				`Sylhet Engineering Collge Undergraduate Result Portal is an online system for result processing built using Django, Celery, ReactJS. This reduced the time required for result creation and publication significantly. This system also eliminated the possibility of calculation errors made by staff.`,
-			logo: "https://static-00.iconduck.com/assets.00/django-icon-1606x2048-lwmw1z73.png",
+			logo: "/static/images/lg.png",
 			linkText: "www.secugrp.com",
 			link: "https:///www.secugrp.com",
 		},
@@ -47,7 +47,7 @@ const INFO = {
 			title: "SEC Clearance Portal",
 			description:
 				"An online-based system for managing student clearance application at Sylhet Engineering College and reducing the hassle for students while applying for clearance. Students apply online for clearance and get it easily upon the approval of the respective authorities.",
-			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+			logo: "/static/images/clr.png",
 			linkText: "www.secclearance.com",
 			link: "https://www.secclearance.com/",
 		},
@@ -58,28 +58,35 @@ const INFO = {
 				`An online classroom system built under a research project of Bangladesh Technical Education Board using
 				Django, Html, CSS and JQuery. This system had been used by Dept. of EEE, Sylhet Engineering College and proved
 				that online classroom systems are good alternative to conventional in-person classroom environments`,
-			logo: "https://static-00.iconduck.com/assets.00/django-icon-1606x2048-lwmw1z73.png",
-			linkText: "View Project",
-			link: "https://github.com",
+			logo: "/static/images/classroom.png",
+			linkText: "flippedclassroom.pythonanywhere.com",
+			link: "https://flippedclassroom.pythonanywhere.com/",
 		},
 
 		{
 			title: "Technoventure 3.0",
 			description:
-				`The official website of Technoventure 3.0 (SEC EEE Fest 2024) using ReactJS, Django, Django Rest
+				`The official website of Technoventure3.0 (SEC EEE Fest 2024) using ReactJS, Django, Django Rest
 				Framework to manage contest registrations.`,
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			logo: "/static/images/WebICon_3.png",
 			linkText: "www.seceeefest.tech",
 			link: "https://www.seceeefest.tech/",
 		},
-
 		{
 			title: "LungSpot",
 			description:
 				"An application for detecting Lung Cancer types with CT Scan images using deep learning CNN Architecture built as a part of my undergraduate thesis.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
+			logo: "/static/images/lungs.svg",
+			linkText: "View Repo",
+			link: "https://github.com/rnium/lungspot_docker",
+		},
+		{
+			title: "rPolls",
+			description:
+				"An online polling platform where anyone can create polls, participate in them. There is also a forum section where users can post or reply",
+			logo: "/static/images/rpolls.png",
+			linkText: "rpolls.sirony.me",
+			link: "https://rpolls.sirony.me/",
 		},
 	],
 };
